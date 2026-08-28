c65212dd406b21eb34eaa8f2cc721b2282c3af06
Ayush Kumar Das <ayushkumard31-cmd@users.noreply.github.com>
2026-08-29 00:09:24 +0530

    Ayush

--- my-code-game/src/app/layout.js
+++ my-code-game/src/app/layout.js
@@ -4,6 +4,7 @@ import FirebaseAnalytics from "./FirebaseAnalytics";
 export const metadata = {
   title: "DSA Dungeon | C Programming Game",
   description: "Master data structures and C programming in a high-stakes coding dungeon.",
+  icons: { icon: "/icon.svg", shortcut: "/icon.svg", apple: "/icon.svg" },
 };
 
 export default function RootLayout({ children }) {
